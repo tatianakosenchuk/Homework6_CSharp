d497cada67b3fc4e86d1d6a12c8e06d9f81294dc
Tatyana <tatiana.kosenchuk@gmail.com>
2022-10-22 21:02:50 +0300

    Homework

---
+++ Zadacha 41/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+int[] GetArrayFromConsole(int length)
+{
+    Console.WriteLine($"Введите {length} элементов массива");
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine() ?? "");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int GetQtyOfPositiveNumbers(int[] arrayOfNums)
+{
+    int quantity = 0;
+for (int i = 0; i < arrayOfNums.Length; i++)
+{
+    if (arrayOfNums[i] > 0) quantity++;
+}
+return quantity;
+}
+
+int [] arrayFromConsole = GetArrayFromConsole(5);
+Console.WriteLine();
+int qty = GetQtyOfPositiveNumbers(arrayFromConsole);
+PrintArray(arrayFromConsole);
+Console.Write($" -> {qty}");
+Console.WriteLine();
---
+++ Zadacha 43/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+double Getnumber(string message)
+{
+    Console.WriteLine(message);
+    double result = double.Parse(Console.ReadLine() ?? "");
+    return result;
+}
+
+(double, double) PointOfLinesCross(double b1, double k1, double b2, double k2)
+{
+    double pointX = (b1 - b2) / (k2 - k1);
+    double pointY = k2 * pointX + b2;
+    return (pointX, pointY);
+}
+
+double b1 = Getnumber("Задайте значение параметра b1");
+double k1 = Getnumber("Задайте значение параметра k1");
+double b2 = Getnumber("Задайте значение параметра b2");
+double k2 = Getnumber("Задайте значение параметра k2");
+(double x, double y) = PointOfLinesCross(b1, k1, b2, k2);
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} прямые пересекаются в точке с координатами ({x}; {y})");
